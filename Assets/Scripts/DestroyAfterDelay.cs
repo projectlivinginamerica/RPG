4d08f34f4d4a4f8f75517cd23dd70538625e1078
Benjamin Wilson <35935536+bennywilson@users.noreply.github.com>
2023-09-23 18:41:52 -0400

    Adding DestroyAfterDelay component

---
+++ Assets/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+
+class DestroyAfterDelay : MonoBehaviour
+{
+    [SerializeField] private float DelayInSeconds;
+
+    void Start()
+    {
+        StartCoroutine(StartDestroyTimer());
+    }
+
+    IEnumerator StartDestroyTimer()
+    {
+        yield return new WaitForSeconds(DelayInSeconds);
+        GameObject.Destroy(gameObject);
+    }
+}
